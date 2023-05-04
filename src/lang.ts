a2d4d5bba93be6a1933fa17cf4f8e36dade25b9f
cql <1373611035@qq.com>
2023-05-04 14:55:34 +0800

    update lang

--- src/lang.ts
+++ src/lang.ts
@@ -1,20 +1,13 @@
 class Lang {
-    static readonly LangCacheKey = '_lang_';
-
     private _lang: string | null = null;
 
     get lang() {
         return this._lang;
     }
 
-    constructor() {
-        this._lang = localStorage.getItem(Lang.LangCacheKey);
-    }
-
     t = (name: string) => name;
 
     change = (langName: string) => {
-        localStorage.setItem(Lang.LangCacheKey, langName);
         this._lang = langName;
     }
 }
