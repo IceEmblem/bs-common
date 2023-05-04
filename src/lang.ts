6a23e9b1038a10c3125de30530bcc5df89200583
cql <1373611035@qq.com>
2023-05-04 14:35:07 +0800

    update lang

--- src/lang.ts
+++ src/lang.ts
@@ -1,9 +1,22 @@
-type Lang = {
-    t: (name: string) => string
+class Lang {
+    static readonly LangCacheKey = '_lang_';
+
+    private _lang: string | null = null;
+
+    get lang() {
+        return this._lang;
+    }
+
+    constructor() {
+        this._lang = localStorage.getItem(Lang.LangCacheKey);
+    }
+
+    t = (name: string) => name;
+
+    change = (langName: string) => {
+        localStorage.setItem(Lang.LangCacheKey, langName);
+        this._lang = langName;
+    }
 }
 
-const lang: Lang = {
-    t: (name) => name
-}
-
-export default lang;
+export default new Lang();
